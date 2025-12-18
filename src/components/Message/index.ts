f3c5f98b6d4d09a3a06de65a1cd52a91c7c05af1
Richbanker <123333089+Richbanker@users.noreply.github.com>
2025-12-18 19:58:26 +0300

    fix: РёСЃРїСЂР°РІР»РµРЅС‹ СЂРµРіРёСЃС‚СЂС‹ РёРјРїРѕСЂС‚РѕРІ РІ РєРѕРјРїРѕРЅРµРЅС‚Р°С… ChatItem, Message, MessageInput

--- src/components/Message/index.ts
+++ src/components/Message/index.ts
@@ -1,2 +1,20 @@
-export { Message } from './Message';
+import Block from "../../services/Block";
+import { messageTemplate } from "./messageTemplate";
 
+interface MessageProps {
+  type: "sent" | "received";
+  content: string;
+  time: string;
+}
+
+class Message extends Block {
+  constructor(props: MessageProps) {
+    super("div", { ...props });
+  }
+
+  protected render() {
+    return this.compile(messageTemplate, this.props);
+  }
+}
+
+export default Message;
